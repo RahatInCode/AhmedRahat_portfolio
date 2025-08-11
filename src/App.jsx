2191d82ac86acfe230868e0ccd0fcdfb5a2d5d79
ahmed <ahmedrahat9901@gmail.com>
2025-08-12 00:57:07 +0600

    deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import Stats from './components/Stats';
+import Achievements from './components/Achievements';
+import ExperienceEducation from './components/ExperienceEducation';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import Navbar from './components/layout/Navbar';
+import AboutMe from './components/layout/AboutMe';
+import Footer from './components/layout/Footer';
 
-import './App.css'
+const App = () => {
+    return (
+  <>
+  <Navbar />
+  <Hero />
+  <AboutMe />
+  <Services />
+  <Portfolio />
+  <Stats />
+  <Achievements />
+  <ExperienceEducation />
+  <Skills />
+  <Testimonials />
+  <Contact />
+  <Blog />
+  <Footer />
+</>
 
-function App() {
+    );
+};
 
-
-  return (
-    <>
-   
-    </>
-  )
-}
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Index from './pages/index.jsx'
 import { BrowserRouter } from 'react-router'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <BrowserRouter>
-     <Index />
-     </BrowserRouter>
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
